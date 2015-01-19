f95d2a8a1de22519f1e444d7a06ebe55c1adbf9b
Etienne Dublé <etienne.duble@imag.fr>
2015-01-19 18:01:28 +0100

    Improved device min-size computation and user information.

--- scripts/live/init/initialize-stick.sh
+++ scripts/live/init/initialize-stick.sh
@@ -5,24 +5,23 @@ source $THIS_DIR/tools.sh
 OLD_ROOT="$1"
 SQUASHFS_CONTENTS_DIR="$2"
 
-verify_space()
-{
-    generate_mtab
-    estimated_size_needed=$(dir_size "$SQUASHFS_CONTENTS_DIR")
-    majorated_size_needed=$((estimated_size_needed*4/3))
-    size_available=$(fs_available_size "$OLD_ROOT")
-    if [ $majorated_size_needed -gt $size_available ]
-    then
-        echo "Sorry - this device is too small. Cannot uncompress the system." 
-        return 1
-    fi
-    return 0
-}
+# get variables such as LVM_VG
+# and DEVICE_MIN_SIZE_KB
+source $OLD_ROOT/dbstck.conf
 
 # find device currently being booted
-source /dbstck.conf     # get LVM_VG value
 set -- $(pvs | grep $LVM_VG)
 device=$(echo $1 | sed -e "s/.$//")
+
+# verify device size
+device_size_kb=$(($(blockdev --getsz $device)/(1024/512)))
+if [ $device_size_kb -lt $DEVICE_MIN_SIZE_KB ]
+then
+    echo "**** ERROR!!!! ******"
+    echo "Sorry - this device is too small! Cannot uncompress the system." 
+    echo "**** ERROR!!!! ******"
+    drop_to_shell_and_halt
+fi
 
 # occupy available space
 echo "Extending disk space..."
@@ -33,7 +32,6 @@ lvextend -l+100%FREE /dev/$LVM_VG/ROOT
 resize2fs /dev/$LVM_VG/ROOT
 
 # extract the squashfs image contents
-verify_space || drop_to_shell_and_halt
 echo "Uncompressing..."
 cp_big_dir_contents $SQUASHFS_CONTENTS_DIR $OLD_ROOT
 
