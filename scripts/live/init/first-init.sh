a4882126a9dd1d2c2e9ce88193b23a8904e03ceb
Etienne Dublé <etienne.duble@imag.fr>
2019-04-03 09:42:56 +0000

    Init procedure now hooked on getty command (was on /sbin/init)
    Fixes #20.
    (debian bug #928080)
    
    The init procedure of debootstick is now hooked on the getty command
    instead of /sbin/init. As a result, this procedure is now called
    when the OS is fully booted.
    This fixes LVM command failures or long timeouts occuring when the OS
    is not booted (no udev daemon, etc).
    
    Moreover, this procedure is now called when user should start to
    interact (login) with the system. So it seems a very suitable place
    to implement user interaction: ask for root password (cf. option
    --config-root-password-first-boot), or allow the user to cancel the
    migration process.

--- scripts/live/init/first-init.sh
+++ scripts/live/init/first-init.sh
@@ -8,17 +8,8 @@ INIT_SCRIPTS_DIR=/opt/debootstick/live/init
 # if error, run a shell
 trap '[ "$?" -eq 0 ] || fallback_sh' EXIT
 
-# we will need /proc and /sys
-[ -e /proc/self ] || mount_proc=1
-[ -e /sys/class ] || mount_sys=1
-[ "$mount_proc" = "1" ] && mount -t proc none /proc
-[ "$mount_sys" = "1" ] && mount -t sysfs none /sys
-
-# remount / read-write
-mount -o remount,rw /
-
-# lvm may need this directory to run properly
-mkdir -p /run/lock
+# this script is now called at the end of the OS bootup procedure,
+# (getty hook), no need to mount/umount filesystems
 
 # ask and set the root password if needed
 if [ "$ASK_ROOT_PASSWORD_ON_FIRST_BOOT" = "1" ]
@@ -37,13 +28,3 @@ fi
 # restore the lvm config as it was in the
 # initial chroot environment
 restore_lvm_conf
-
-# cleanup
-[ "$mount_proc" = "1" ] && umount /proc
-[ "$mount_sys" = "1" ] && umount /sys
-
-# restore and start the usual init
-rm /sbin/init
-mv /sbin/init.orig /sbin/init
-exec /sbin/init $*
-
